86ad4c3946330eef0c2eb3981914ffdc849d3d95
slimtimtam <ltara8@vt.edu>
2018-12-04 01:43:42 -0500

    for u purni

--- src/java/edu/vt/pojo/Flight.java
+++ src/java/edu/vt/pojo/Flight.java
@@ -10,4 +10,39 @@ package edu.vt.pojo;
  */
 public class Flight {
     
+    private String ticketUrl;
+    private String cost;
+    private String airline;
+
+    public Flight(String ticketUrl, String cost) {
+        this.ticketUrl = ticketUrl;
+        this.cost = cost;
+    }
+
+    public String getTicketUrl() {
+        return ticketUrl;
+    }
+
+    public void setTicketUrl(String ticketUrl) {
+        this.ticketUrl = ticketUrl;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+    
+    
+    
 }
